0607ad5121528571b2e40cfc62c61ef5e73ceb60
kripticni <kripticno@gmail.com>
2025-08-22 00:11:13 +0200

    slstatus: added the code for the nvidia patch

---
+++ slstatus/components/gpu.c
@@ -0,0 +1,54 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "nvml.h"
+#include "../util.h"
+
+static nvmlDevice_t device = NULL;
+static int nvml_initialized = 0;
+
+static int
+nvml_init_once(void)
+{
+    if (nvml_initialized)
+        return 1;
+
+    if (nvmlInit() != NVML_SUCCESS)
+        return 0;
+
+    if (nvmlDeviceGetHandleByIndex(0, &device) != NVML_SUCCESS) {
+        nvmlShutdown();
+        return 0;
+    }
+
+    nvml_initialized = 1;
+    return 1;
+}
+
+const char *
+gpu_perc(const char* unused)
+{
+    nvmlUtilization_t utilization;
+
+    if (!nvml_init_once())
+        return NULL;
+
+    if (nvmlDeviceGetUtilizationRates(device, &utilization) != NVML_SUCCESS)
+        return NULL;
+
+    return bprintf("%d", utilization.gpu);
+}
+
+const char *
+gpu_temp(const char* unused)
+{
+    unsigned int temp;
+
+    if (!nvml_init_once())
+        return NULL;
+
+    if (nvmlDeviceGetTemperature(device, NVML_TEMPERATURE_GPU, &temp) != NVML_SUCCESS)
+        return NULL;
+
+    return bprintf("%d", temp);
+}
+
